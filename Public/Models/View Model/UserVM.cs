f51bb9be16bd6e547f137808846b284b57738e90
Beata <beatapodgajski@gmail.com>
2022-09-13 21:40:11 +0200

    .

---
+++ Public/Models/View Model/UserVM.cs
@@ -0,0 +1,16 @@
+﻿using ClassLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Public.Models.View_Model
+{
+    public class UserVM
+    {
+        public User User { get; set; }
+        public IList<ApartmentReview> Reviews { get; set; }
+        public IList<ApartmentReservation> Reservations { get; set; }
+
+    }
+}
